6f02a307121bf239fa997ebbc2a32ee590da6371
Haris Amin <aminharis7@gmail.com>
2016-01-06 01:51:30 -0500

    Hurray we can now clients can send and receive text frames (that are not continuous) without issue :)

--- main.swift
+++ main.swift
@@ -79,26 +79,152 @@ public class WebSocketServer: CWServerDelegate {
     			print("Request is not websocket: \(requestHeader)")
     		}
     	} else{
-    		print("NO DATA STRING FROM TCP: \(data)")
-	        let frame = WebSocketFrame(opCode: .Text, data: Array("ALT WS: WILL THIS WORK!!!".utf8) )
-	        let frameData = frame.getData()
+            let count = data.length / sizeof(UInt8)
+            var dataArray = [UInt8](count: count, repeatedValue: 0)
+            // copy bytes into array
+            data.getBytes(&dataArray, length:count * sizeof(UInt8))
 
-	        let data = NSData(bytes: frameData, length: frameData.count)
+            let fin = dataArray[0] & WebSocketFrame.FinMask != 0
+            let rsv1 = dataArray[0] & WebSocketFrame.Rsv1Mask != 0
+            let rsv2 = dataArray[0] & WebSocketFrame.Rsv2Mask != 0
+            let rsv3 = dataArray[0] & WebSocketFrame.Rsv3Mask != 0
 
-            do {
-				// try connection.writeData(data)
-                for con in websocketConnections {
-                    try con.writeData(data)
+            // guard let opCode = WebSocketFrame.OpCode(rawValue: dataArray[0] & WebSocketFrame.OpCodeMask) else { print("WE DO NOT HAVE PROPER FRAME!!!") }
+            let opCode = WebSocketFrame.OpCode(rawValue: dataArray[0] & WebSocketFrame.OpCodeMask)
+
+            let masked = dataArray[1] & WebSocketFrame.MaskMask != 0
+            let payloadLength = dataArray[1] & WebSocketFrame.PayloadLenMask
+
+            var headerExtraLength = masked ? sizeof(UInt32) : 0
+            if payloadLength == 126 {
+                headerExtraLength += sizeof(UInt16)
+            } else if payloadLength == 127 {
+                headerExtraLength += sizeof(UInt64)
+            }
+
+            if opCode!.isControl {
+                print("**** WE HAVE A CONTROL FRAME!!!!")
+            } else {
+                print("** WE DO NOT HAVE A CONTROL FRAME!!!!")
+            }
+
+            switch opCode! {
+            case .Text:
+                print("*** WE HAVE TEXT FRAME ***")
+                var _textFramePayload: [UInt8]
+                // guard let maskKey = frame.maskKey else { return -1 }
+                let maskOffset = max(Int(headerExtraLength) - 4, 2)
+                print("maskOffset: \(maskOffset)")
+                let maskKey = Array(dataArray[maskOffset ..< maskOffset+4])
+
+                // guard maskKey.count == 4 else { return fail("maskKey wrong length") }
+
+                let consumeLength = min(UInt64(payloadLength), UInt64(dataArray.count))
+                _textFramePayload = []
+                var maskKeyIndex = 0
+                var idx = 0
+                for byte in dataArray {
+                    if idx < 6 {
+                        idx += 1
+                        continue
+                    }
+                    _textFramePayload.append(byte ^ maskKey[maskKeyIndex])
+                    if (maskKeyIndex + 1) < maskKey.count {
+                        maskKeyIndex += 1
+                    } else {
+                        maskKeyIndex = 0
+                    }
                 }
-			}
-			catch {
-				print("Failed to make SEND MESSAGE")
-			}
+
+
+                let str = String.fromBytes(_textFramePayload)
+                let textFrame = WebSocketFrame(opCode: .Text, data: Array(str.utf8))
+                let frameData = textFrame.getData()
+                let textData = NSData(bytes: frameData, length: frameData.count)
+                do {
+                    for con in websocketConnections {
+                        try con.writeData(textData)
+                    }
+                }
+                catch {
+                    print("Failed to make SEND MESSAGE")
+                }
+            case .Continuation:
+                print("*** WE HAVE Continuation FRAME ***")
+            case .Binary:
+                print("***WE HAVE Binary FRAME")
+            case .Ping:
+                print("*** WE HAVE PING FRAME ***")
+            case .Pong:
+                print("*** WE HAVE PONG FRAME ***")
+            case .Close:
+                print("*** WE HAVE CLOSE FRAME ***")
+            default:
+                print("*** WE HAVE SOM OTHER FRAME ***")
+            }
     	}
     }
     public func stopped(server: CWSocketServer){
     	print("stopped: \(server)")
     }
+
+    // private func sendFrame(data: NSData, opcode: WebSocketFrame.OpCode) -> NSMutableData {
+    //     print("************************** COME HERE *********************************")
+    //     var headerBytes = [UInt8](count: 2, repeatedValue: 0)
+    //     headerBytes[0] |= WebSocketFrame.FinMask
+    //     headerBytes[0] |= (WebSocketFrame.OpCodeMask & opcode.rawValue)
+
+    //     if data.length < 126 {
+    //         headerBytes[1] |= UInt8(data.length)
+    //     } else if UInt(data.length) <= UInt(UInt16.max) {
+    //         headerBytes[1] |= UInt8(126)
+    //         var length = UInt16(bigEndian: UnsafePointer<UInt16>(data.bytes)[0])
+    //         let nsData = NSData(bytes: &length, length: sizeof(UInt16))
+    //         var bytes = [UInt8](count: sizeof(UInt16), repeatedValue: 0)
+    //         nsData.getBytes(&bytes, length: bytes.count)
+    //         headerBytes.appendContentsOf(bytes)
+    //     } else {
+    //         headerBytes[1] |= UInt8(127)
+    //         var length = UInt64(bigEndian: UnsafePointer<UInt64>(data.bytes)[0])
+    //         let nsData = NSData(bytes: &length, length: sizeof(UInt64))
+    //         var bytes = [UInt8](count: sizeof(UInt64), repeatedValue: 0)
+    //         nsData.getBytes(&bytes, length: bytes.count)
+    //         headerBytes.appendContentsOf(bytes)
+    //     }
+
+    //     headerBytes[1] |= WebSocketFrame.MaskMask
+    //     var maskKey = [UInt8](count: 4, repeatedValue: 0)
+    //     // SecRandomCopyBytes(kSecRandomDefault, UInt(maskKey.count), &maskKey)
+    //     headerBytes.appendContentsOf(maskKey)
+
+    //     var payloadBytes = [UInt8](count: data.length, repeatedValue: 0)
+    //     data.getBytes(&payloadBytes, length: payloadBytes.count)
+
+    //     var realBytes = [UInt8](count: 9, repeatedValue: 0)
+    //     var i = 0
+    //     for index in 6..<payloadBytes.count {
+    //         realBytes[i] = payloadBytes[index] ^ maskKey[index % 4]
+    //         print("did \(i)")
+    //         i += 1
+    //     }
+    //     // write(NSData(bytes: &headerBytes, length: headerBytes.count))
+    //     // write(NSData(bytes: &payloadBytes, length: payloadBytes.count))
+    //     let frameData: NSMutableData = NSMutableData()
+    //     frameData.appendBytes(&headerBytes, length: headerBytes.count)
+    //     // frameData.appendBytes(&payloadBytes, length: payloadBytes.count)
+    //     frameData.appendBytes(&realBytes, length: realBytes.count)
+
+    //     // if let str = NSString(bytes: payloadBytes, length: payloadBytes.count, encoding: NSUTF8StringEncoding) as? String {
+    //     //     print("THIS IS STRING: \(str)")
+    //     // } else {
+    //     //     print("not a valid UTF-8 sequence")
+    //     // }
+    //     // var str = String(bytes: payloadBytes, encoding: NSUTF8StringEncoding)
+    //     print(" *** payloadBytes: \(payloadBytes)")
+    //     print(" *** realBytes: \(realBytes)")
+    //     // print("**** OK THIS IS STRING: \(str) *****")
+    //     return frameData
+    // }
 }
 
 let wsServer = WebSocketServer()
