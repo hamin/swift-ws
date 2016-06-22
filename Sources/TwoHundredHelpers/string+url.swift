23c19a1d7e9de4fd019df46a0f4a4fd5669d9249
Haris Amin <aminharis7@gmail.com>
2016-06-22 17:20:05 -0400

    Lots more cleanup

--- Sources/TwoHundredHelpers/string+url.swift
+++ Sources/TwoHundredHelpers/string+url.swift
@@ -1,106 +1,106 @@
+////
+////  string+url.swift
+////  twohundred
+////
+////  Created by Johannes Schriewer on 24/11/15.
+////  Copyright © 2015 Johannes Schriewer. All rights reserved.
+////
+//import Foundation
 //
-//  string+url.swift
-//  twohundred
+///// URL encoding and decoding support
+//public extension String {
 //
-//  Created by Johannes Schriewer on 24/11/15.
-//  Copyright © 2015 Johannes Schriewer. All rights reserved.
+//    /// URL-encode string
+//    ///
+//    /// - returns: URL-Encoded version of the string
+//    public func urlEncodedString() -> String {
+//        var result = ""
+//        var gen = self.unicodeScalars.makeIterator()
 //
-import Foundation
-
-/// URL encoding and decoding support
-public extension String {
-
-    /// URL-encode string
-    ///
-    /// - returns: URL-Encoded version of the string
-    public func urlEncodedString() -> String {
-        var result = ""
-        var gen = self.unicodeScalars.makeIterator()
-
-        while let c = gen.next() {
-            switch c {
-            case " ": // Space
-                result.append("%20")
-            case "!": // !
-                result.append("%21")
-            case "\"": // "
-                result.append("%22")
-            case "#": // #
-                result.append("%23")
-            case "$": // $
-                result.append("%24")
-            case "%": // %
-                result.append("%25")
-            case "&": // &
-                result.append("%26")
-            case "'": // '
-                result.append("%27")
-            case "(": // (
-                result.append("%28")
-            case ")": // )
-                result.append("%29")
-            case "*": // *
-                result.append("%2A")
-            case "+": // +
-                result.append("%2B")
-            case ",": // ,
-                result.append("%2C")
-            case "/": // /
-                result.append("%2F")
-            case ":": // :
-                result.append("%3A")
-            case ";": // ;
-                result.append("%3B")
-            case "=": // =
-                result.append("%3D")
-            case "?": // ?
-                result.append("%3F")
-            case "@": // @
-                result.append("%40")
-            case "[": // [
-                result.append("%5B")
-            case "\\": // \
-                result.append("%5C")
-            case "]": // ]
-                result.append("%5D")
-            case "{": // {
-                result.append("%7B")
-            case "|": // |
-                result.append("%7C")
-            case "}": // }
-                result.append("%7D")
-            default:
-                result.append(c)
-            }
-        }
-        return result
-    }
-
-    /// URL-decode string
-    ///
-    /// - returns: Decoded version of the URL-Encoded string
-    public func urlDecodedString() -> String {
-        var result = ""
-        var gen = self.unicodeScalars.makeIterator()
-
-        while let c = gen.next() {
-            switch c {
-            case "%":
-                // get 2 chars
-                if let c1 = gen.next() {
-                    if let c2 = gen.next() {
-                        if let c = UInt32("\(c1)\(c2)", radix: 16) {
-                            result.append(UnicodeScalar(c))
-                        } else {
-                            result.append("%\(c1)\(c2)")
-                        }
-                    }
-                }
-            default:
-                result.append(c)
-            }
-        }
-
-        return result
-    }
-}
+//        while let c = gen.next() {
+//            switch c {
+//            case " ": // Space
+//                result.append("%20")
+//            case "!": // !
+//                result.append("%21")
+//            case "\"": // "
+//                result.append("%22")
+//            case "#": // #
+//                result.append("%23")
+//            case "$": // $
+//                result.append("%24")
+//            case "%": // %
+//                result.append("%25")
+//            case "&": // &
+//                result.append("%26")
+//            case "'": // '
+//                result.append("%27")
+//            case "(": // (
+//                result.append("%28")
+//            case ")": // )
+//                result.append("%29")
+//            case "*": // *
+//                result.append("%2A")
+//            case "+": // +
+//                result.append("%2B")
+//            case ",": // ,
+//                result.append("%2C")
+//            case "/": // /
+//                result.append("%2F")
+//            case ":": // :
+//                result.append("%3A")
+//            case ";": // ;
+//                result.append("%3B")
+//            case "=": // =
+//                result.append("%3D")
+//            case "?": // ?
+//                result.append("%3F")
+//            case "@": // @
+//                result.append("%40")
+//            case "[": // [
+//                result.append("%5B")
+//            case "\\": // \
+//                result.append("%5C")
+//            case "]": // ]
+//                result.append("%5D")
+//            case "{": // {
+//                result.append("%7B")
+//            case "|": // |
+//                result.append("%7C")
+//            case "}": // }
+//                result.append("%7D")
+//            default:
+//                result.append(c)
+//            }
+//        }
+//        return result
+//    }
+//
+//    /// URL-decode string
+//    ///
+//    /// - returns: Decoded version of the URL-Encoded string
+//    public func urlDecodedString() -> String {
+//        var result = ""
+//        var gen = self.unicodeScalars.makeIterator()
+//
+//        while let c = gen.next() {
+//            switch c {
+//            case "%":
+//                // get 2 chars
+//                if let c1 = gen.next() {
+//                    if let c2 = gen.next() {
+//                        if let c = UInt32("\(c1)\(c2)", radix: 16) {
+//                            result.append(UnicodeScalar(c))
+//                        } else {
+//                            result.append("%\(c1)\(c2)")
+//                        }
+//                    }
+//                }
+//            default:
+//                result.append(c)
+//            }
+//        }
+//
+//        return result
+//    }
+//}
--- Sources/swift-ws/WebSocketServer.swift
+++ Sources/swift-ws/WebSocketServer.swift
@@ -1,7 +1,7 @@
 import Foundation
 import TwoHundredHelpers
-import CryptoEssentials
-import SHA1
+
+import SwiftSockets
 
 #if os(Linux)
     import Glibc
@@ -9,160 +9,204 @@ import SHA1
     import Darwin.C
 #endif
 
-// public class WebSocketServer: CWServerDelegate {
-//     var socketServer: CWSocketServer?
-//     var websocketConnections: [CWServerConnection] = []
+func + (lhs: NSData, rhs: NSData) -> NSData {
+    let data = NSMutableData(data: lhs)
+    data.append(rhs)
+    return data
+}
 
-//     init(port:Int = 8080) {
-//         socketServer = CWSocketServer (port: UInt16(port), socketFamily: CWSocketFamily.v4)
-//         socketServer?.delegate = self
-//     }
-
-//     public func startWS(){
-//         do {
-//             try self.socketServer!.start()
-//         }
-//         catch {
-//             print(error)
-//         }
-//     }
-
-//     // MARK: - CWServerDelegate
-//     public func connected(connection: CWServerConnection){
-//         print("CONNECTED: \(connection)")
-//     }
-//     public func disconnected(connection: CWServerConnection){
-//         print("DISCONNECTED: \(connection)")
-//         // TODO need to remove object, needs CWServerConnection needs to conform to either Equatable/Hashable
-//     }
-//     public func hasData(connection: CWServerConnection){
-//         print("hasData: \(connection)")
-
-//         let data: NSMutableData = NSMutableData()
-//         connection.readData(data: data)
-
-//         if let datastring = NSString(data: data, encoding:NSUTF8StringEncoding),
-//             let requestHeader = RequestHeader(data:datastring as String){
-//             if requestHeader.isWebSocket {
-//                 print("requestheaders: \(requestHeader.headers)")
-
-//                 do {
-//                     let resp = requestHeader.websocketHandhsakeUpgradeReponse()
-//                     let hsSockData = resp.handshakeUpgradeMakeSocketData()
-//                     let hsSockString = hsSockData.stringValue()
-//                     try connection.write(st: hsSockString!)
-//                     websocketConnections.append(connection)
-//                 }
-//                 catch {
-//                     print("Failed to make HandShake")
-//                 }
-//             }else{
-//                 print("Request is not websocket: \(requestHeader)")
-//             }
-//         } else{
-//             let count = data.length / sizeof(UInt8)
-//             var dataArray = [UInt8](repeating:0, count: count)
-//             // copy bytes into array
-//             data.getBytes(&dataArray, length:count * sizeof(UInt8))
-
-//             let fin = dataArray[0] & WebSocketFrame.FinMask != 0
-//             let rsv1 = dataArray[0] & WebSocketFrame.Rsv1Mask != 0
-//             let rsv2 = dataArray[0] & WebSocketFrame.Rsv2Mask != 0
-//             let rsv3 = dataArray[0] & WebSocketFrame.Rsv3Mask != 0
-
-//             // guard let opCode = WebSocketFrame.OpCode(rawValue: dataArray[0] & WebSocketFrame.OpCodeMask) else { print("WE DO NOT HAVE PROPER FRAME!!!") }
-//             let opCode = WebSocketFrame.OpCode(rawValue: dataArray[0] & WebSocketFrame.OpCodeMask)
-
-//             let masked = dataArray[1] & WebSocketFrame.MaskMask != 0
-//             let payloadLength = dataArray[1] & WebSocketFrame.PayloadLenMask
-
-//             var headerExtraLength = masked ? sizeof(UInt32) : 0
-//             if payloadLength == 126 {
-//                 headerExtraLength += sizeof(UInt16)
-//             } else if payloadLength == 127 {
-//                 headerExtraLength += sizeof(UInt64)
-//             }
-
-//             if opCode!.isControl {
-//                 print("**** WE HAVE A CONTROL FRAME!!!!")
-//             } else {
-//                 print("** WE DO NOT HAVE A CONTROL FRAME!!!!")
-//             }
-
-//             switch opCode! {
-//             case .Text:
-//                 print("*** WE HAVE TEXT FRAME ***")
-//                 var _textFramePayload: [UInt8]
-//                 // guard let maskKey = frame.maskKey else { return -1 }
-//                 let maskOffset = max(Int(headerExtraLength) - 4, 2)
-//                 print("maskOffset: \(maskOffset)")
-//                 let maskKey = Array(dataArray[maskOffset ..< maskOffset+4])
-
-//                 // guard maskKey.count == 4 else { return fail("maskKey wrong length") }
-
-//                 let consumeLength = min(UInt64(payloadLength), UInt64(dataArray.count))
-//                 _textFramePayload = []
-//                 var maskKeyIndex = 0
-//                 var idx = 0
-//                 for byte in dataArray {
-//                     if idx < 6 {
-//                         idx += 1
-//                         continue
-//                     }
-//                     _textFramePayload.append(byte ^ maskKey[maskKeyIndex])
-//                     if (maskKeyIndex + 1) < maskKey.count {
-//                         maskKeyIndex += 1
-//                     } else {
-//                         maskKeyIndex = 0
-//                     }
-//                 }
-
-
-//                 //                let str = String.fromBytes(bytes: _textFramePayload)
-//                 let str = String(bytes: _textFramePayload, encoding: NSUTF8StringEncoding)
-//                 let textFrame = WebSocketFrame(opCode: .Text, data: Array(str!.utf8))
-//                 let frameData = textFrame.getData()
-//                 let textData = NSData(bytes: frameData, length: frameData.count)
-//                 do {
-//                     for con in websocketConnections {
-//                         try con.writeData(data: textData)
-//                     }
-//                 }
-//                 catch {
-//                     print("Failed to make SEND MESSAGE")
-//                 }
-//             case .Continuation:
-//                 print("*** WE HAVE Continuation FRAME ***")
-//             case .Binary:
-//                 print("***WE HAVE Binary FRAME")
-//             case .Ping:
-//                 print("*** WE HAVE PING FRAME ***")
-
-//                 // let count = data.length / sizeof(UInt8)
-//                 // var dataArray = [UInt8](count: count, repeatedValue: 0)
-//                 // // copy bytes into array
-//                 // data.getBytes(&dataArray, length:count * sizeof(UInt8))
-
-//                 // let pongFrame = WebSocketFrame(opCode: .Pong, data: dataArray)
-//                 // let frameData = pongFrame.getData()
-//                 // let pongData =  NSData(bytes: frameData, length: frameData.count)
-//                 do {
-//                     try connection.writeData(data: data)
-//                     print("Sent Pong Message!")
-//                 }
-//                 catch {
-//                     print("Failed to make SEND PONG")
-//                 }
-//             case .Pong:
-//                 print("*** WE HAVE PONG FRAME ***")
-//             case .Close:
-//                 print("*** WE HAVE CLOSE FRAME ***")
-//             default:
-//                 print("*** WE HAVE SOM OTHER FRAME ***")
-//             }
-//         }
-//     }
-//     public func stopped(server: CWSocketServer){
-//         print("stopped: \(server)")
-//     }
-// }
+public class WebSocketServer {
+//    var socketServer: SocketServer = SwiftSocketServer()
+    var connections: Set<SwiftSocket> = Set()
+    var wsPort: Int = 8080
+    
+    private var socket: PassiveSocketIPv4!
+    private var receivedRequestCallback: ReceivedRequestCallback?
+    private var websocketRequestCallBack: WebsocketRequestCallback?
+    
+    init(port:Int) {
+        self.wsPort = port
+        self.receivedRequestCallback = self.httpRequestCallback()
+        self.websocketRequestCallBack = self.websocketRequestCallback()
+    }
+    
+    public func start() throws {
+        guard let socket = PassiveSocketIPv4(address: sockaddr_in(port: self.wsPort)) else { throw SocketErrors.ListenError }
+        _ = socket.listen(queue: dispatch_get_global_queue(0, 0)) {
+            socket in
+            
+            _ = socket.onRead {
+                newsock, length in
+                
+                socket.isNonBlocking = true
+                
+                var initialData: NSData?
+                var bodyData: NSData?
+                
+                let (size, data, _) = newsock.read()
+                
+                if size > 0 {
+                    initialData = NSData(bytes: data, length: size)
+                }
+                
+                guard initialData != nil else {
+                    // We are dealing with disconnection or bad requests
+                    self.connections.remove(SwiftSocket(socket: socket))
+                    socket.close()
+                    print("Client closed ||| Current Connections: \(self.connections.count)")
+                    return
+                }
+                
+                if let _ = NSString(data: initialData!, encoding: NSUTF8StringEncoding) {
+                    print("should be upgrade request")
+                } else {
+                    _ = self.websocketRequestCallBack?( initialData!, SwiftSocket(socket: socket) )
+                    return
+                }
+                
+                if let initialData = initialData {
+                    let request = Request(headerData: initialData)
+                    
+                    // Initial data may not contain body
+                    // Check if request contains a body, and that it hasn't been read yet
+                    if  let lengthString = request.headers["Content-Length"],
+                        let length = UInt(lengthString) where length > 0 && request.bodyString == nil {
+                        
+                        let (bSize, bData, _) = newsock.read()
+                        
+                        if bSize > 0 {
+                            bodyData = NSData(bytes: bData, length: bSize)
+                            request.parseBodyData(d: bodyData)
+                        }
+                    }
+                    
+                    _ = self.receivedRequestCallback?(request, SwiftSocket(socket: socket))
+                }
+            }
+        }
+        
+        self.socket = socket
+    }
+    
+    public func disconnect() {
+        for swiftSocket in self.connections {
+            swiftSocket.socket.close()
+        }
+        self.socket.close()
+    }
+    
+    
+    // Private methods
+    
+    private func handleUpgradeRequest(socket: SwiftSocket, request: Request) {
+        print("GONNA HANDLE REQUEST")
+        if request.isWebSocket == true {
+            let resp = request.websocketHandhsakeUpgradeReponse()
+            resp.handshakeUpgradeMakeSocketData()
+            let respData = resp.bodyData
+            socket.sendData(close:false, data: respData)
+            print("WE HAVE WEBSOCKET UPGRADE!!!!")
+            self.connections.insert(socket)
+        } else {
+            print("Request is not websocket!!!")
+        }
+    }
+    
+    private func httpRequestCallback() -> ReceivedRequestCallback {
+        return {
+            request, socket in
+            self.handleUpgradeRequest(socket: socket, request: request)
+            return true
+        }
+    }
+    
+    // Main Websocket Handling logic
+    private func websocketRequestCallback() -> WebsocketRequestCallback {
+        return {
+            data, socket in
+            
+            let count = data.length / sizeof(UInt8)
+            var dataArray = [UInt8](repeating:0, count: count)
+            // copy bytes into array
+            data.getBytes(&dataArray, length:count * sizeof(UInt8))
+            
+            let fin = dataArray[0] & WebSocketFrame.FinMask != 0
+            let rsv1 = dataArray[0] & WebSocketFrame.Rsv1Mask != 0
+            let rsv2 = dataArray[0] & WebSocketFrame.Rsv2Mask != 0
+            let rsv3 = dataArray[0] & WebSocketFrame.Rsv3Mask != 0
+            
+            // guard let opCode = WebSocketFrame.OpCode(rawValue: dataArray[0] & WebSocketFrame.OpCodeMask) else { print("WE DO NOT HAVE PROPER FRAME!!!") }
+            let opCode = WebSocketFrame.OpCode(rawValue: dataArray[0] & WebSocketFrame.OpCodeMask)
+            
+            let masked = dataArray[1] & WebSocketFrame.MaskMask != 0
+            let payloadLength = dataArray[1] & WebSocketFrame.PayloadLenMask
+            
+            var headerExtraLength = masked ? sizeof(UInt32) : 0
+            if payloadLength == 126 {
+                headerExtraLength += sizeof(UInt16)
+            } else if payloadLength == 127 {
+                headerExtraLength += sizeof(UInt64)
+            }
+            
+            if opCode!.isControl {
+                print("**** WE HAVE A CONTROL FRAME!!!!")
+            } else {
+                print("** WE DO NOT HAVE A CONTROL FRAME!!!!")
+            }
+            
+            switch opCode! {
+            case .Text:
+                print("*** WE HAVE TEXT FRAME ***")
+                var _textFramePayload: [UInt8]
+                // guard let maskKey = frame.maskKey else { return -1 }
+                let maskOffset = max(Int(headerExtraLength) - 4, 2)
+                print("maskOffset: \(maskOffset)")
+                let maskKey = Array(dataArray[maskOffset ..< maskOffset+4])
+                
+                // guard maskKey.count == 4 else { return fail("maskKey wrong length") }
+                
+                let consumeLength = min(UInt64(payloadLength), UInt64(dataArray.count))
+                _textFramePayload = []
+                var maskKeyIndex = 0
+                var idx = 0
+                for byte in dataArray {
+                    if idx < 6 {
+                        idx += 1
+                        continue
+                    }
+                    _textFramePayload.append(byte ^ maskKey[maskKeyIndex])
+                    if (maskKeyIndex + 1) < maskKey.count {
+                        maskKeyIndex += 1
+                    } else {
+                        maskKeyIndex = 0
+                    }
+                }
+                
+                
+                //                let str = String.fromBytes(bytes: _textFramePayload)
+                let str = String(bytes: _textFramePayload, encoding: NSUTF8StringEncoding)
+                let textFrame = WebSocketFrame(opCode: .Text, data: Array(str!.utf8))
+                let frameData = textFrame.getData()
+                let textData = NSData(bytes: frameData, length: frameData.count)
+                
+                for con in self.connections {
+                    con.sendData(close: false, data: textData)
+                }
+            case .Continuation:
+                print("*** WE HAVE Continuation FRAME ***")
+            case .Binary:
+                print("***WE HAVE Binary FRAME")
+            case .Ping:
+                print("*** WE HAVE PING FRAME ***")
+                socket.sendData(close: false, data: data)
+            case .Pong:
+                print("*** WE HAVE PONG FRAME ***")
+            case .Close:
+                print("*** WE HAVE CLOSE FRAME ***")
+            }
+            
+            
+            return true
+        }
+    }
+}
